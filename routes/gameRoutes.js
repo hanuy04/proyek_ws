29c214943241fe6495107185d2166db770a27544
Andreas <vinswork18@gmail.com>
2024-06-14 13:02:38 +0700

    Update gameRoutes.js

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { addGame } = require("../controllers/gameController");
+const { addGame, updateGames } = require("../controllers/gameController");
 const { getGames } = require("../controllers/gameController");
 const { verifyToken } = require("../middleware/verifyJWT");
 const router = express.Router();
 
+// Post
 router.post("/addGame/:game_id", verifyToken, addGame);
+
+// Get
 router.get("/getGames", getGames);
+
+// Put
+router.put("/updateGames/:game_id", verifyToken, updateGames);
+// router.put("/updateTeam", verifyToken, updateTeam);
+// router.put("/updateMatch", verifyToken, updateMatch);
+// router.put("/updateTicket", verifyToken, updateTicket); // Blm
 
 module.exports = router;
